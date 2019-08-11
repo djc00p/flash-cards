5520890ad30f8f32c3f2c4deecd51bd7e907c5e9
Deonte Cooper <deonte.cooper12@gmail.com>
2019-08-11 11:00:43 -0600

    Add Test for Turn class has a guess

--- test/test_turn.py
+++ test/test_turn.py
@@ -13,3 +13,9 @@ class TestTurn(unittest.TestCase):
         turn = Turn("Juneau", card)
 
         self.assertEqual(turn.card, card)
+
+    def test_turn_has_a_guess(self):
+        card = Card("What is the capital of Alaska?", "Juneau", 'Geography')
+        turn = Turn("Juneau", card)
+
+        self.assertEqual(turn.guess, "Juneau")
