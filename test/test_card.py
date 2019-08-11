b845e50a172fff5a141e5cf162747a3b71a1312d
Deonte Cooper <deonte.cooper12@gmail.com>
2019-08-11 10:41:44 -0600

    Add test for Card instance variable answer

--- test/test_card.py
+++ test/test_card.py
@@ -8,6 +8,11 @@ class TestCard(unittest.TestCase):
         pass
 
     def test_card_has_a_question(self):
-        card = Card("What is the capital of Alaska?")
+        card = Card("What is the capital of Alaska?", "Juneau", 'Geography')
         q = card.question
         self.assertEqual(q,"What is the capital of Alaska?")
+
+    def test_card_has_an_answer(self):
+        card = Card("What is the capital of Alaska?", "Juneau", 'Geography')
+        a = card.answer
+        self.assertEqual(a,"Juneau")
