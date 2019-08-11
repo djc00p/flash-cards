da7341be2d1c3a4719cc3983bb3d9bdcab5c8a01
Deonte Cooper <deonte.cooper12@gmail.com>
2019-08-11 12:50:01 -0600

    Add test for Deck method cards_in_category

--- test/test_deck.py
+++ test/test_deck.py
@@ -27,3 +27,13 @@ class TestTurn(unittest.TestCase):
         deck = Deck(cards)
 
         self.assertEqual(deck.count(), 3)
+
+    def test_deck_has_a_card_count(self):
+        card1 = Card("What is the capital of Alaska?", "Juneau", 'Geography')
+        card2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM")
+        card3 = Card("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", "STEM")
+
+        cards = [card1, card2, card3]
+        deck = Deck(cards)
+        # import pdb; pdb.set_trace()
+        self.assertEqual(deck.cards_in_category("STEM"), [card2, card3])
