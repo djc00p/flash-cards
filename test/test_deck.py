24a634d964c4bb9a93882f511840b83cd72ee0e4
Deonte Cooper <deonte.cooper12@gmail.com>
2019-08-11 12:11:17 -0600

    Add test for Deck method count

--- test/test_deck.py
+++ test/test_deck.py
@@ -8,7 +8,7 @@ class TestTurn(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_turn_has_a_card(self):
+    def test_deck_has_a_cards(self):
         card1 = Card("What is the capital of Alaska?", "Juneau", 'Geography')
         card2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", 'STEM')
         card3 = Card("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", "STEM")
@@ -17,3 +17,13 @@ class TestTurn(unittest.TestCase):
         deck = Deck(cards)
 
         self.assertEqual(deck.cards, [card1, card2, card3])
+
+    def test_deck_has_a_card_count(self):
+        card1 = Card("What is the capital of Alaska?", "Juneau", 'Geography')
+        card2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", 'STEM')
+        card3 = Card("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", "STEM")
+
+        cards = [card1, card2, card3]
+        deck = Deck(cards)
+
+        self.assertEqual(deck.count(), 3)
