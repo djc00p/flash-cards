e684d9ce8b3777b4dba6042286006818b5396fbe
Deonte Cooper <deonte.cooper12@gmail.com>
2019-08-20 11:44:08 -0600

    Add take turn method to round

--- lib/round.py
+++ lib/round.py
@@ -1,5 +1,13 @@
+from .turn import Turn
+
 class Round(object):
     """docstring for Round."""
 
     def __init__(self, deck):
         self.deck = deck
+        self.turns = []
+        self.current_card = deck.cards[0]
+
+    def take_turn(self, guess):
+        turn = Turn(guess, self.current_card)
+        return turn
